7c38ed964cc69963c76f547789b0a2d1123552f2
Xc9crazy <69mhrnda@gmail.com>
2026-01-28 10:49:42 +0900

    初回

---
+++ public/signup_finish.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>会員登録完了</title>
+  <link rel="stylesheet" href="/style.css">
+</head>
+<body>
+
+<h1>会員登録完了</h1>
+
+会員登録が完了しました。<br>
+登録した内容をもとに<a href="/login.php">ログイン</a>してください。
+
+</body>
+</html>
